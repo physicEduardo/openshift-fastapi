212c9471fabce2277b38192556f1a1245ea715e2
physicEduardo <physiceduardo@phs.phs.com>
2023-02-22 15:47:19 -0500

    included V2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def index():
 
 @app.get('/message')
 def index():
-    return {'data': 'FastAPI is great!'}
+    return {'data': 'FastAPI is great V2!'}
 
 if __name__ == "__main__":
     uvicorn.run(app, host='0.0.0.0', port=8080)
